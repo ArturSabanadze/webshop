789c3e0809ab1729547ef46e32f1af0297541d3d
Artur Sabanadze <diakosmisi26@hotmail.com>
2025-12-07 21:21:57 +0100

    update2

--- Frontend/src/Functions/css_loader.php
+++ Frontend/src/Functions/css_loader.php
@@ -3,4 +3,5 @@ $css_loader = "<link rel=\"stylesheet\" href=\"styles/index.css\">
 <link rel=\"stylesheet\" href=\"styles/navbar.css\">
 <link rel=\"stylesheet\" href=\"styles/header.css\">
 <link rel=\"stylesheet\" href=\"styles/main.css\">
-<link rel=\"stylesheet\" href=\"styles/footer.css\">";
+<link rel=\"stylesheet\" href=\"styles/footer.css\">
+<link rel=\"stylesheet\" href=\"styles/register.css\">";
--- Frontend/src/Includes/navigationbar.php
+++ Frontend/src/Includes/navigationbar.php
@@ -23,18 +23,43 @@
             <a href="index.php?page=categories">CATEGORIES</a>
             <a href="index.php?page=about">ABOUT</a>
         </div>
-        <div>
-            <a href="login.php" title="Login" class="dsk-login-btn">
-                Login<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
-                    <!-- Door -->
-                    <path d="M15 3h4v18h-4" />
-                    <!-- Arrow entering -->
-                    <path d="M10 17l5-5-5-5" />
-                    <path d="M15 12H3" />
-                </svg>
-            </a>
-        </div>
+        <?php if (!isset($_SESSION['username'])): ?>
+            <div>
+                <a href="index.php?page=login" title="Login" class="dsk-login-btn">
+                    Login<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
+                        <!-- Door -->
+                        <path d="M15 3h4v18h-4" />
+                        <!-- Arrow entering -->
+                        <path d="M10 17l5-5-5-5" />
+                        <path d="M15 12H3" />
+                    </svg>
+                </a>
+                <a href="index.php?page=register" title="Sign Up" class="dsk-login-btn">
+                    Sign Up
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
+                        <!-- Plus icon for signing up -->
+                        <path d="M12 5v14" />
+                        <path d="M5 12h14" />
+                    </svg>
+                </a>
+            </div>
+        <?php else: ?>
+            <div>
+                <a href="logout.php" title="Logout" class="dsk-login-btn">
+                    Logout
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
+                        <!-- Door -->
+                        <path d="M15 3h4v18h-4" />
+                        <!-- Arrow exiting -->
+                        <path d="M15 12H3" />
+                        <path d="M10 7l5 5-5 5" />
+                    </svg>
+                </a>
+            </div>
+        <?php endif; ?>
     </navbar>
 
     <!-- Slide-out mobile menu -->

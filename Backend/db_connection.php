828ea7428b48ba3e4046051918cb1cee5d1c2485
Artur Sabanadze <diakosmisi26@hotmail.com>
2025-12-08 11:25:33 +0100

    arturs branch update

--- Backend/db_connection.php
+++ Backend/db_connection.php
@@ -1,9 +1,11 @@
 <?php
+$env = parse_ini_file(__DIR__ . '/.env');
 // DB-Zugangsdaten
-$host = "localhost";
-$dbname = "webshop";
-$user = "root";
-$pass = "";
+$host = $env['DB_HOST'] ?? 'localhost';
+$dbname = $env['DB_NAME'] ?? 'webshop';
+$user = $env['DB_USER'] ?? 'root';
+$pass = $env['DB_PASS'] ?? '';
+
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
 
@@ -13,9 +15,9 @@ try {
         $user,
         $pass,
         [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Fehler als Exceptions
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Standard Fetch
-            PDO::ATTR_PERSISTENT => false                 // keine persistente Verbindung
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_PERSISTENT => false
         ]
     );
 } catch (PDOException $e) {

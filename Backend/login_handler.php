9987cc475980201301e5bf5ed1dc941ab15ba943
Artur Sabanadze <diakosmisi26@hotmail.com>
2025-12-10 14:00:22 +0100

    css styling and some page links fixing

--- Backend/login_handler.php
+++ Backend/login_handler.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && ($_POST['action'] ?? '') === 'login
 
         if ($user) {
             if (password_verify($password, $user['password_hash'])) {
-                // Erfolgreicher Login
+                // Erfolgreicher Login = Session token und Benutzerdaten setzen
                 $token = bin2hex(random_bytes(16));
                 $_SESSION['token'] = $token;
                 $_SESSION['user_id'] = $user['id'];

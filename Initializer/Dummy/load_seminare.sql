45f4fe39fb86aa6df1633b289358c09b790111b1
Artur Sabanadze <diakosmisi26@hotmail.com>
2026-01-15 10:51:21 +0100

    clean unnecesary files from repository

--- Initializer/Dummy/load_seminare.sql
+++
@@ -1,7 +0,0 @@
-LOAD DATA LOCAL INFILE 'products_for_xampp.csv' 
-INTO TABLE products
-FIELDS TERMINATED BY ';'
-OPTIONALLY ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES
-(product_name, image_url, description, price, min_capacity, max_capacity, start_date, end_date);
